1eb0a97a4574c192ac9859da1060bb1902b2abe4
Milan <mariasi.milan@gmail.com>
2018-05-15 20:32:45 +0200

    Unittesting example.

---
+++ InquisitorGame.py
@@ -0,0 +1,6 @@
+class InquisitorGame:
+    """"This is the main class of the game."""
+
+    def __init__(self):
+        """"Constructor"""
+        print("Starting a new Game")
---
+++ Start.py
@@ -0,0 +1,4 @@
+from InquisitorGame import InquisitorGame
+
+
+game = InquisitorGame()
